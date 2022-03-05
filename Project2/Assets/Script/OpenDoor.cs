45288dd64f9d99830c14cff36b537c9671260776
lq455 <lq455@nyu.edu>
2022-03-05 16:21:37 +0400

    interaction

---
+++ Project2/Assets/Script/OpenDoor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject door;
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Key")){
+            door.SetActive(false);
+        }
+    }
+}
